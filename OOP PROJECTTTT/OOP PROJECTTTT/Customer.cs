4ae05da226057bfb4dd8814db43dd0e52ed03f3d
Luqmanzi <luqmannh45@gmail.com>
2024-10-03 15:10:40 +0800

    Add files via upload

---
+++ OOP PROJECTTTT/OOP PROJECTTTT/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FloralFusion
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Customer(int customerId, string name)
+        {
+            CustomerID = customerId;
+            Name = name;
+            Orders = new List<Order>();
+        }
+    }
+}
---
+++ OOP PROJECTTTT/OOP PROJECTTTT/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloralFusion
+{
+    public class Transaction
+    {
+        public int TransactionID { get; set; }
+        public double Amount { get; set; }
+
+        public Transaction(int transactionId, double amount)
+        {
+            TransactionID = transactionId;
+            Amount = amount;
+        }
+    }
+}
