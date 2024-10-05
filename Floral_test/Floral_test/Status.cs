8e7cca93a1e118d128ba84d331771cd0e26112e0
AmirulAdam143 <amiruladam65@gmail.com>
2024-10-05 14:27:02 +0800

    Miro_UML

---
+++ Floral_test/Floral_test/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Status
+{
+	public Status()
+	{
+		public string Pending { get; set; }
+		public string OnGoing { get; set; }
+		public string Completed { get; set; }	
+	}
+}
