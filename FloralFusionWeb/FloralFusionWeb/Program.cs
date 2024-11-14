27bb838b3b13254cf8613f4dddcb3557356eab9e
amiera-bat <amiera.batrisyia05@gmail.com>
2024-11-14 19:49:53 +0800

    Fixed error in Program.cs

--- FloralFusionWeb/FloralFusionWeb/Program.cs
+++ FloralFusionWeb/FloralFusionWeb/Program.cs
@@ -7,5 +7,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7260/") });
+builder.Services.AddSingleton<StringLibrary.FlowerShopBusiness>();
 
 await builder.Build().RunAsync();
